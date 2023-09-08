a3bed6656e2421c91db3f7416259bc4ba649ee5a
juansu01 <thejuank0@hotmail.com>
2023-09-08 14:48:58 -0500

    Add validation and register controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { Post, Body } from '@nestjs/common';
+
+import { UserService } from 'src/user/user.service';
+import { CreateUser } from 'src/user/dtos/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() newUser: CreateUser) {
+    return this.userService.create(newUser);
+  }
+}
