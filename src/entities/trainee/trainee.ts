1af849fd9e2f4ad2fcbef73ec6f315f68a6b518f
juansu01 <thejuank0@hotmail.com>
2023-09-08 13:16:59 -0500

    Add ManyToOne relationship between trainees and trainer

--- src/entities/trainee/trainee.ts
+++ src/entities/trainee/trainee.ts
@@ -1,11 +1,15 @@
-import { Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 
 import { BaseEntity } from '../base-entity.entity';
 import { User } from '../user/user.entity';
+import { Trainer } from '../trainer/trainer';
 
 @Entity()
 export class Trainee extends BaseEntity {
   @OneToOne(() => User)
   @JoinColumn()
   user: User;
+
+  @ManyToOne(() => Trainer, (trainer) => trainer.trainees)
+  trainer: Trainer;
 }
--- src/entities/trainer/trainer.ts
+++ src/entities/trainer/trainer.ts
@@ -1,11 +1,15 @@
-import { Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 
 import { BaseEntity } from '../base-entity.entity';
 import { User } from '../user/user.entity';
+import { Trainee } from '../trainee/trainee';
 
 @Entity()
 export class Trainer extends BaseEntity {
   @OneToOne(() => User)
   @JoinColumn()
   user: User;
+
+  @OneToMany(() => Trainee, (trainee) => trainee.trainer)
+  trainees: Trainee[];
 }
