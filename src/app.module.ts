3253bc864dcadf6193bdb2cc6d3df145175efdf2
Juansu01 <thejuank0@hotmail.com>
2023-11-05 14:41:00 -0500

    Using db connection with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './configs/database.config';
+import { getDatabaseConfig } from './configs/database.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(databaseConfig), AuthModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => getDatabaseConfig(),
+    }),
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -3,12 +3,14 @@ import { User } from 'src/entities/user/user.entity';
 import { Trainer } from 'src/entities/trainer/trainer';
 import { Trainee } from 'src/entities/trainee/trainee';
 
-export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'kratitos10',
-  database: 'fitness-app-dev',
-  entities: [User, Trainee, Trainer],
-  synchronize: true,
+export const getDatabaseConfig = (): TypeOrmModuleOptions => {
+  return {
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [User, Trainee, Trainer],
+    synchronize: true,
+  };
 };
